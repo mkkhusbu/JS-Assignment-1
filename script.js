19af6447925cb853426e52ee4f82def5357a05b2
mkkhusbu <arifajmalkhusbu@gmail.com>
2024-08-27 22:47:35 +0600

    JS Assignment

---
+++ script.js
@@ -0,0 +1,92 @@
+//1 no.
+let carName = "Volvo";
+console.log(carName);
+
+
+//2 no.
+// let firstName = "John";
+// let lastName = "Doe";
+// let age = 35;
+// console.log(firstName + " " + lastName + " " + age);
+
+
+
+//3 no.
+// let x = 10;
+// let y = 5;
+// x *= y;
+// console.log(x);
+
+
+
+//4 no.
+// let length = 16; // length is a number
+// console.log(typeof length)
+
+// let lastName  = "Johnson"; // lastName is a string 
+// console.log(typeof lastName);
+
+// const x = {
+//     firstName: "John",
+//     lastName: "Doe"
+// }
+// console.log(typeof x);
+//x is a object
+
+
+
+//5 no.
+// function myFunction() {
+//     alert("Hello World!");
+// }
+// myFunction();
+
+
+
+//6 no.
+// const person = {
+//     name : "John",
+//     age: 50
+// }
+// alert(person.name + " is " + person.age);
+
+
+//7 no. 
+// function clickMe(){
+//     alert("Hello world!");
+// }
+
+
+
+//8 no. 
+// const cars = ["Volvo", "Jeep", "Mercedes"];
+// alert(cars.length);
+
+// const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+// Brand[0] = "Ford";
+// console.log(Brand);
+
+
+
+//9 no.
+// let randomNumber = Math.random();
+// console.log(randomNumber);
+
+// function highest(){
+//    return Math.max(10,20);
+// }
+// console.log(highest());
+
+// let num = 9;
+// let result = Math.sqrt(num);
+// console.log(result);
+
+
+
+//10 no.
+// let x = 10;
+// let y = 5;
+// alert(x > y);
+
+// let age = 18;
+// alert( (age < 18) ? "Too young" : "Old enough" );
